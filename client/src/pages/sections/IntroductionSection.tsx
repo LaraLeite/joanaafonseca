98af7d4a5b71e289dbd5e90e3609d84415376d02
Lara <larasilvaleite@gmail.com>
2026-04-12 21:19:32 +0100

    Update Text IntroductionSection.tsx

--- client/src/pages/sections/IntroductionSection.tsx
+++ client/src/pages/sections/IntroductionSection.tsx
@@ -16,10 +16,9 @@ export const IntroductionSection = (): JSX.Element => {
               bem estar mental
             </h2>
             <p className="texto-lg [font-family:'Literata',Helvetica] font-normal text-white/85">
-              O meu nome é <strong className="font-semibold">Joana Afonseca</strong>, Psicóloga Clínica e da Saúde, aqui para ajudá-la a ultrapassar
-              os seus desafios e a encontrar um novo equilíbrio emocional.
-              Acredito numa abordagem integrativa e personalizada, para que cada pessoa encontre
-              o seu caminho de crescimento e bem-estar.
+              O meu nome é <strong className="font-semibold">Joana Afonseca</strong>, sou Psicóloga Clínica, e o meu propósito é acolher e caminhar ao lado de quem procura transformação, oferecendo 
+              um espaço seguro onde cada pessoa se possa sentir verdadeiramente compreendida. Acredito numa abordagem integrativa e personalizada, que respeita a singularidade de cada história e de 
+              cada percurso. Com presença, empatia e cuidado, procuro apoiar cada pessoa a encontrar o seu próprio caminho de crescimento, equilíbrio e bem-estar.”
             </p>
           </FadeUp>
 
