f60d9f9fdc136b3d4eb4906a89ec6dc43fba26e5
luiseduardopita <luis.eduardo.pita@gmail.com>
2026-04-21 22:15:11 +0100

    Update ContactoPage.tsx
    Apaguei título horário e centrei o ícone de relógio

--- client/src/pages/ContactoPage.tsx
+++ client/src/pages/ContactoPage.tsx
@@ -65,10 +65,8 @@ export const ContactoPage = (): JSX.Element => {
              <img 
                 src="/figmaAssets/icons/icon-horario.svg" 
                 alt="Horário"
-                className="w-5 h-5 mt-[4px]"
+                className="w-5 h-5 mx-auto"
              />
-            <p className="[font-family:'Literata',Helvetica] font-semibold text-textoparagraph texto-sl">
-              Horário:
             </p>
             <p className="[font-family:'Literata',Helvetica] font-normal text-textoparagraph texto-sl leading-[26px]">
               Segunda a Sexta-feira: 09:00-19:00h
